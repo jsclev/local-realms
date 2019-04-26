0ddcc1e3d9d1ae5a04061d33c846d557d2123519
John Cleveland <john.s.cleveland@gmail.com>
2019-04-26 11:12:44 -0400

    Added a few more stores.

--- scripts/us/a.py
+++ scripts/us/a.py
@@ -10,6 +10,22 @@ from apps.finder.models import Store
 ###############################################################################
 # Business
 ###############################################################################
+business = Business(name='Amazing Discoveries')
+business.website = 'www.amazingmtg.com'
+business.facebook = 'www.facebook.com/AmazingDiscoveriesCG'
+business.save()
+
+store = Store(business=business, city='Casa Grande', state_code='AZ', zip_code='85122')
+store.status = settings.GLOBAL_CONSTANTS['STATUS_OPEN']
+store.street1 = '204 N Sacaton St'
+store.latitude = 32.877465
+store.longitude = -111.755649
+store.phone = '520-274-9294'.replace('-', '')
+store.save()
+
+###############################################################################
+# Business
+###############################################################################
 business = Business(name='Friction Games')
 business.website = 'www.frictiongamesaz.com'
 business.facebook = 'www.facebook.com/frictiongamesaz'
@@ -21,6 +37,33 @@ store.street1 = '52035 Arizona St'
 store.latitude = 31.557008
 store.longitude = -110.352997
 store.phone = '520-559-2202'.replace('-', '')
+store.save()
+
+###############################################################################
+# Business
+###############################################################################
+business = Business.objects.get(name='Amazing Discoveries')
+
+store = Store(business=business, city='Gilbert', state_code='AZ', zip_code='85234')
+store.status = settings.GLOBAL_CONSTANTS['STATUS_OPEN']
+store.street1 = '745 N Gilbert Rd'
+store.street2 = 'Ste 116'
+store.latitude = 33.363882
+store.longitude = -111.787992
+store.phone = '520-460-6891'.replace('-', '')
+store.save()
+
+###############################################################################
+# Business
+###############################################################################
+business = Business.objects.get(name='Amazing Discoveries')
+
+store = Store(business=business, city='Tucson', state_code='AZ', zip_code='85719')
+store.status = settings.GLOBAL_CONSTANTS['STATUS_OPEN']
+store.street1 = '2410 E Broadway Blvd'
+store.latitude = 32.221302
+store.longitude = -110.936149
+store.phone = '520-320-0338'.replace('-', '')
 store.save()
 
 ###############################################################################
@@ -42,6 +85,9 @@ store.longitude = -110.866797
 store.phone = '520-603-4037'.replace('-', '')
 store.save()
 
+###############################################################################
+# Business
+###############################################################################
 business = Business.objects.get(name='Tucson Games and Gadgets')
 
 store = Store(business=business, city='Tucson', state_code='AZ', zip_code='85705')
