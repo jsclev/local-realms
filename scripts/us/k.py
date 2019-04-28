35237e7859f05352c49191d99a8cfedd8ea7e004
John Cleveland <john.s.cleveland@gmail.com>
2019-04-28 10:42:30 -0400

    Added a couple more stores.

--- scripts/us/k.py
+++ scripts/us/k.py
@@ -25,6 +25,22 @@ store.save()
 ###############################################################################
 # Business
 ###############################################################################
+business = Business(name="The Dragon's Hoard")
+business.website = 'dragonshoardllc.com/'
+business.facebook = 'www.facebook.com/TheDragonsHoardLLC'
+business.save()
+
+store = Store(business=business, city='Lawrence', state_code='KS', zip_code='66044')
+store.status = settings.GLOBAL_CONSTANTS['STATUS_OPEN']
+store.street1 = '1045 Pennsylvania St'
+store.latitude = 38.963988
+store.longitude = -95.229401
+store.phone = '785-766-9608'.replace('-', '')
+store.save()
+
+###############################################################################
+# Business
+###############################################################################
 business = Business(name='Gatekeeper Hobbies')
 business.website = 'www.gatekeeperhobbies.com'
 business.facebook = 'www.facebook.com/GatekeeperHobbies'
@@ -37,4 +53,37 @@ store.street1 = '4123 SW Gage Center Dr'
 store.latitude = 39.042176
 store.longitude = -95.727563
 store.phone = '785-232-3429'.replace('-', '')
+store.save()
+
+###############################################################################
+# Business
+###############################################################################
+business = Business(name="Grog's Games, Play Space and Board Game Club")
+business.facebook = 'www.facebook.com/grogsgames'
+business.save()
+
+store = Store(business=business, city='Topeka', state_code='KS', zip_code='66614')
+store.status = settings.GLOBAL_CONSTANTS['STATUS_OPEN']
+store.street1 = '3124 SW 29th St'
+store.street2 = '#8'
+store.latitude = 39.015418
+store.longitude = -95.716992
+store.phone = '785-221-7164'.replace('-', '')
+store.save()
+
+###############################################################################
+# Business
+###############################################################################
+business = Business(name="Huscarl Hobbies and Games")
+business.website = 'www.huscarlhobbiesandgames.com'
+business.facebook = 'www.facebook.com/HuscarlHobbies'
+business.email = 'contactus@huscarlhobbiesandgames.com'
+business.save()
+
+store = Store(business=business, city='Topeka', state_code='KS', zip_code='66604')
+store.status = settings.GLOBAL_CONSTANTS['STATUS_OPEN']
+store.street1 = '1930 SW Westport Dr'
+store.latitude = 39.031604
+store.longitude = -95.756133
+store.phone = '785-215-6545'.replace('-', '')
 store.save()
