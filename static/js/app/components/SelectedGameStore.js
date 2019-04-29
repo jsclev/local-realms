3ec04de52ce043ba1704ce902a3888b0dd795873
Timothy Cleveland <tjcp2@k12albemarle.org>
2019-04-28 21:03:51 -0400

    unfinished changes about selected store

--- static/js/app/components/SelectedGameStore.js
+++ static/js/app/components/SelectedGameStore.js
@@ -6,11 +6,29 @@ Vue.component('SelectedGameStore', {
         <div id="selected-game-store" 
              class=""
              v-if="selectedGameStore">
-            <div>Name: {{ selectedGameStore.business.name }}</div>
-            <div>Address: {{ selectedGameStore.street1 }} + {{ selectedGameStore.city }}</div>
-            <div>Website: {{ selectedGameStore.business.website }}</div>
-            <div>Email: {{ selectedGameStore.business.email }}</div>
-            <div>Phone: {{ selectedGameStore.phone }}</div>
+            <div>{{ selectedGameStore.business.name }}</div>
+            <div class="info-list-container">
+                <div class=""></div>
+                <div class="info-text">
+                    {{ selectedGameStore.street1 }}<br/>{{ selectedGameStore.city }}, {{ selectedGameStore.stateCode }} {{ selectedGameStore.zipCode }}
+                </div>
+            </div>
+            <a :href="'http://' + selectedGameStore.business.website" target="_blank" style="text-decoration: none">
+                <div id="website" class="info-list-container">
+                    <div class="website-icon"></div> 
+                    <span id="website-text" class="info-text">{{ selectedGameStore.business.website }}</span>
+                </div>
+            </a>
+            <div class="info-list-container">
+                <div class="info-text">
+                    Email: {{ selectedGameStore.business.email }}
+                </div>
+            </div>
+            <div class="info-list-container">
+                <div class="info-text">
+                    {{ selectedGameStore.phone }}
+                </div>
+            </div>
         </div>`,
     computed: {
         selectedGameStore() {
