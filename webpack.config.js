32609de94bda68157fd6babadc91d73c406936f0
John Cleveland <john.s.cleveland@gmail.com>
2019-05-04 13:39:04 -0400

    Moved some CSS into the single file components.

--- webpack.config.js
+++ webpack.config.js
@@ -15,15 +15,18 @@ module.exports = {
         filename: 'main.js'
     },
     module: {
-        rules: [
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader'
-            }
-        ]
+        rules: [{
+            test: /\.vue$/,
+            loader: 'vue-loader'
+        }, {
+            test: /\.css$/,
+            use: [
+                'vue-style-loader',
+                'css-loader'
+            ]
+        }]
     },
     plugins: [
-        // make sure to include the plugin!
         new VueLoaderPlugin()
     ],
     watch: true
