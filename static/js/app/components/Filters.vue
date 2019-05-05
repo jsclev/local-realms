ec5c05d125ea44f953ed08df6ef2de6995b8022d
Timothy Cleveland <tjcp2@k12albemarle.org>
2019-05-05 10:31:39 -0400

    changes to filters

--- static/js/app/components/Filters.vue
+++ static/js/app/components/Filters.vue
@@ -4,7 +4,11 @@
             <button class="filter-item">{{ tag.name }}</button>
             <div class="check-mark check-icon-show"></div>
         </div>
-        <button id=""></button>
+        <div class="content">
+            <div class="number-filters">0 filters</div>
+            <button class="cancel" ripple="ripple">CANCEL</button>
+            <button class="apply" ripple="ripple">APPLY</button>
+        </div>
     </div>
 </template>
 
@@ -27,6 +31,7 @@
 
             $(document).ready(function () {
                 $('.tag-filter').click(function () {
+                    $('#save-changes').addClass('save-changes-allowed');
                     var currentID = this.id;
                     currentID = $('[id="' + currentID + '"]');
                     if ($(currentID).hasClass('checked')) {
@@ -62,7 +67,63 @@
                 searchContainer.removeClass("input-common-filter");
                 searchOuter.removeClass("search-outer-after-filter");
                 $('#filter-dim').removeClass('dimmed');
+            });
+            $('.cancel').click(function () {
+                $('#filters').removeClass('filter-expanded');
+                list.removeClass("entity-list-transition-filter");
+                searchContainer.removeClass("input-common-filter");
+                searchOuter.removeClass("search-outer-after-filter");
+                $('#filter-dim').removeClass('dimmed');
             })
+            // Ripple effect
+            (function () {
+                let cleanUp, debounce, i, len, ripple, rippleContainer, ripples, showRipple;
+
+                debounce = function (func, delay) {
+                    let inDebounce;
+                    inDebounce = undefined;
+                    return function () {
+                        let args, context;
+                        context = this;
+                        args = arguments;
+                        clearTimeout(inDebounce);
+                        return inDebounce = setTimeout(function () {
+                            return func.apply(context, args);
+                        }, delay);
+                    };
+                };
+
+                showRipple = function (e) {
+                    let pos, ripple, rippler, size, style, x, y;
+                    ripple = this;
+                    rippler = document.createElement('span');
+                    size = ripple.offsetWidth;
+                    pos = ripple.getBoundingClientRect();
+                    x = e.pageX - pos.left - (size / 2);
+                    y = e.pageY - pos.top - (size / 2);
+                    style = 'top:' + y + 'px; left: ' + x + 'px; height: ' + size + 'px; width: ' + size + 'px;';
+                    ripple.rippleContainer.appendChild(rippler);
+                    return rippler.setAttribute('style', style);
+                };
+
+                cleanUp = function () {
+                    while (this.rippleContainer.firstChild) {
+                        this.rippleContainer.removeChild(this.rippleContainer.firstChild);
+                    }
+                };
+
+                ripples = document.querySelectorAll('[ripple]');
+
+                for (i = 0, len = ripples.length; i < len; i++) {
+                    ripple = ripples[i];
+                    rippleContainer = document.createElement('div');
+                    rippleContainer.className = 'ripple--container';
+                    ripple.addEventListener('mousedown', showRipple);
+                    ripple.addEventListener('mouseup', debounce(cleanUp, 2000));
+                    ripple.rippleContainer = rippleContainer;
+                    ripple.appendChild(rippleContainer);
+                }
+            }());
         },
         computed: {
             tags: function () {
