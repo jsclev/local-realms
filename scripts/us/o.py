7e902854e56a176324152ee70f438aaf2ea574a2
John Cleveland <john.s.cleveland@gmail.com>
2019-05-08 12:19:02 -0400

    Added some more stores.

--- scripts/us/o.py
+++ scripts/us/o.py
@@ -10,6 +10,21 @@ from apps.finder.models import Store
 ###############################################################################
 # Business
 ###############################################################################
+business = Business.objects.create(name='Battleground Games And Hobbies')
+business.facebook = 'www.facebook.com/FantasyGameCenter'
+business.save()
+
+store = Store(business=business, city='Port Clinton', state_code='OH', zip_code='43452')
+store.status = settings.GLOBAL_CONSTANTS['STATUS_OPEN']
+store.street1 = '114 E Perry St'
+store.latitude = 41.512950
+store.longitude = -82.940332
+store.phone = '419-341-8753'.replace('-', '')
+store.save()
+
+###############################################################################
+# Business
+###############################################################################
 business = Business.objects.create(name='Frogtown Hobbies')
 
 store = Store(business=business, city='Rossford', state_code='OH', zip_code='43460')
