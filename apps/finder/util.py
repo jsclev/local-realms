231ac970989641ab3de3ccd6d3cd0996be1ed52d
Patrick Gorman <gormanpe@vcu.edu>
2019-05-09 00:23:17 -0400

    Created function to remove chain businesses from queryset

---
+++ apps/finder/util.py
@@ -0,0 +1,7 @@
+
+forbidden_stores = [
+    "Gamestop",
+    "GameStop",
+    "Barnes & Noble",
+    "Barnes & Noble Booksellers"
+    ]
--- apps/finder/views.py
+++ apps/finder/views.py
@@ -7,6 +7,7 @@ from apps.finder.models import Business
 from apps.finder.serializers import BusinessSerializer
 from apps.finder.models import Tag
 from apps.finder.serializers import TagSerializer
+from apps.finder.util import forbidden_stores
 
 
 def get_home(request):
@@ -18,6 +19,10 @@ def get_home(request):
 
 def get_shops(request):
     businesses = Business.objects.all().order_by('name')
+    for business in businesses:
+        if business.name in forbidden_stores:
+            businesses.exclude(name=business.name)
+            print('%s has a forbidden name.  Removing from queryset.', business.name)
     serializer = BusinessSerializer(businesses, many=True)
 
     return JsonResponse(serializer.data, safe=False)
