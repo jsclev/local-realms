2d31500819a1c66a2d23f71298897c9e8d44c4c0
John Cleveland <john.s.cleveland@gmail.com>
2019-05-12 20:40:33 -0400

    Added a robots.txt file.

--- settings/urls.py
+++ settings/urls.py
@@ -1,9 +1,14 @@
+from django.conf.urls import url
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
 
 from apps.finder import views
 
 urlpatterns = [
+    url(r'^robots.txt',
+        lambda x: HttpResponse("User-Agent: *\nDisallow:", content_type="text/plain"),
+        name="robots_file"),
     path('admin/', admin.site.urls),
     path('', views.get_home),
     path('about/', views.get_about),
