c14aea2f474e384a095121a8dd9a2d0b4037ea08
Patrick Gorman <gormanpe@vcu.edu>
2019-05-28 10:18:48 -0400

    Created option for user email input

---
+++ apps/accounts/util.py
@@ -0,0 +1,16 @@
+from django import forms
+
+from django.contrib.auth.forms import UserCreationForm
+from django.utils.translation import gettext, gettext_lazy as _
+
+
+class UserCreationFormWithEmail(UserCreationForm):
+    email = forms.EmailField(label=_("Email"), max_length=254)
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
